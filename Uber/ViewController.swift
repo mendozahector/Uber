112fc868c1f65c6bf35c532b8dd76032b2754701
Hector Mendoza <hectormendoza@byui.edu>
2018-11-02 09:28:39 -0400

    Initial Commit

---
+++ Uber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uber
+//
+//  Created by Hector Mendoza on 11/2/18.
+//  Copyright © 2018 Hector Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
